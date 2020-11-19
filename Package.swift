9a0190f88e02530cfa4f5c38f9fa6af8a819ea27
vvveiii <cntrump@gmail.com>
2020-11-20 07:01:51 +0800

    lowercase package name

--- Package.swift
+++ Package.swift
@@ -4,22 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "YYJson",
+    name: "yyjson",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "YYJson",
-            targets: ["YYJson"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "yyjson",
+            targets: ["yyjson"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YYJson",
+            name: "yyjson",
             path: ".",
             sources: ["src"],
             publicHeadersPath: "src"
