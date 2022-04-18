d960b38a5e07648c8f89d064f9137b3728886e22
Pavel Otchertsov <pavel.otchertsov@gmail.com>
2022-04-18 17:23:13 +0300

    Fix doc variable naming in doc_mut_copy
    Since introduction of `yyjson_mut_doc_mut_copy` function,
    the variable name `i_doc` lost its purpose, since `i` in the name
    means `immutable`, but this is false for `yyjson_mut_doc`.
    
    Rename `i_doc` to `doc` to match function declarations in `yyjson.h`
    and fix this broken logic in function naming.
    
    It could possible be changed to `m_doc` in `yyjson_mut_doc_mut_copy`,
    but this is already used for internal variable, so change it to `doc` instead.

--- src/yyjson.c
+++ src/yyjson.c
@@ -1229,15 +1229,15 @@ yyjson_mut_doc *yyjson_mut_doc_new(const yyjson_alc *alc) {
     return doc;
 }
 
-yyjson_api yyjson_mut_doc *yyjson_doc_mut_copy(yyjson_doc *i_doc,
+yyjson_api yyjson_mut_doc *yyjson_doc_mut_copy(yyjson_doc *doc,
                                                const yyjson_alc *alc) {
     yyjson_mut_doc *m_doc;
     yyjson_mut_val *m_val;
     
-    if (!i_doc || !i_doc->root) return NULL;
+    if (!doc || !doc->root) return NULL;
     m_doc = yyjson_mut_doc_new(alc);
     if (!m_doc) return NULL;
-    m_val = yyjson_val_mut_copy(m_doc, i_doc->root);
+    m_val = yyjson_val_mut_copy(m_doc, doc->root);
     if (!m_val) {
         yyjson_mut_doc_free(m_doc);
         return NULL;
@@ -1246,15 +1246,15 @@ yyjson_api yyjson_mut_doc *yyjson_doc_mut_copy(yyjson_doc *i_doc,
     return m_doc;
 }
 
-yyjson_api yyjson_mut_doc *yyjson_mut_doc_mut_copy(yyjson_mut_doc *i_doc,
+yyjson_api yyjson_mut_doc *yyjson_mut_doc_mut_copy(yyjson_mut_doc *doc,
                                                    const yyjson_alc *alc) {
     yyjson_mut_doc *m_doc;
     yyjson_mut_val *m_val;
     
-    if (!i_doc || !i_doc->root) return NULL;
+    if (!doc || !doc->root) return NULL;
     m_doc = yyjson_mut_doc_new(alc);
     if (!m_doc) return NULL;
-    m_val = yyjson_mut_val_mut_copy(m_doc, i_doc->root);
+    m_val = yyjson_mut_val_mut_copy(m_doc, doc->root);
     if (!m_val) {
         yyjson_mut_doc_free(m_doc);
         return NULL;
